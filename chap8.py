1c77fe8e0ea963a34a2209e65412efb467e0b44a
itraa <itraa.narayan@gmail.com>
2013-08-21 23:37:11 -0400

    dev-sprint3 commit

--- chap8.py
+++ chap8.py
@@ -1,2 +1,19 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Aarthi Narayan
+
+
+def rotate_word(word,n):
+    i = 0
+    new_word = ""
+    if len(word) > 0:
+        while i < len(word):
+            word_num=ord(word[i]) - ord('a')
+            word_num = (word_num + n)  % 26 + ord('a')
+#            if word_num > ord('z'):            
+#                word_num = ord('a') + n                
+            new_word=new_word + chr(word_num) 
+            i = i + 1
+        return new_word
+
+print rotate_word('cheer',7)
+print rotate_word('melon', -10)
--- chap9.py
+++ chap9.py
@@ -1,2 +1,56 @@
 # This is where the answers to Chapter 9 questions for the BSS Dev RampUp go
-# Name:
+# Name: Aarthi Narayan
+# Exercise 9.1
+
+fin = open('words.txt')
+for line in fin:
+    word = line.strip()
+    if len(word) > 20:
+        print word
+
+
+# Exercise 9.2
+
+def has_no_e(word):
+    for letter in word:
+        if letter == 'e':
+            return False
+    return True
+
+count = 0
+n = 0
+fin = open('words.txt')
+for line in fin:
+    word = line.strip()
+    n = n + 1
+    if has_no_e(word) is True:
+        print word
+        count = count + 1
+print "Percentage is: " + str(100 * (float(count)/float(n)))    
+
+# Exercise 9.3
+
+n= 0
+fin = open('words.txt')
+string = raw_input('Enter the string: ')
+
+def avoids(word,string):
+    for letter in word:
+        if letter in string:
+            return False  
+    return True
+
+for line in fin:
+    word = line.strip()
+    if avoids(word,string) is True:
+        n = n + 1
+print "Number of words without string: " + str(n)
+
+
+# Exercise 9.4
+
+def uses_only(word,string):
+    for letter in word:
+        if letter not in string:
+            return False
+    return True
